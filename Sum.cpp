a395ffdd992092e8497cd99191e6a4c1ad58ed7d
Divyesh-Sew <dvysewratan@gmail.com>
2024-11-23 22:29:44 -0300

    Sum of numbers
    Opdracht som van nummers.

---
+++ Sum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int firstnumber;
+    int secondnumber;
+    cout << "Type your first number:";
+    cin >> firstnumber;
+    cout << "Type your second number:";
+    cin >> secondnumber;
+    cout << "The sum of your numbers is:  " << firstnumber + secondnumber;
+    return 0;
+}
