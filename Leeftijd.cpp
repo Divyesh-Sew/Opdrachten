d492562e4eb718d3a0e2d163909bb47180d83edf
Divyesh-Sew <dvysewratan@gmail.com>
2024-11-26 18:08:31 -0300

    leeftijd assignment

---
+++ Leeftijd.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int geboortedatum;
+    cout << "Wat is jouw geboorte jaar ? ";
+    cin >> geboortedatum;
+    cout << "Jouw leeftijd is: " << 2024 - geboortedatum;
+
+}
