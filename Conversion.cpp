416253c2da8e0d930fb3ea9a4057cbae524c3975
Divyesh-Sew <dvysewratan@gmail.com>
2024-11-26 18:17:01 -0300

    Conversion C to F

---
+++ Conversion.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int celcius;
+    cout << "Voer een temperatuur waarde van Celcius in: ";
+    cin >> celcius;
+    cout << (celcius * 9/5) + 32 << " Farenheit";
+
+}
